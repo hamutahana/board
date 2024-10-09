5d7100e6c34afe1c9d1de676b01166433123f1ae
akihisa <sahikia117@gmail.com>
2024-10-09 18:09:39 +0900

    コメント保存処理とコメント表示の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,24 @@
 class CommentsController < ApplicationController
   def create
+		comment = Comment.new(comment_params)
+		if comment.save
+			redirect_to comment.board, flash: {
+				notice: "コメントを投稿しました"
+			}
+		else
+			redirect_back fallback_location: comment.board, flash: {
+				comment: comment,
+				error_messages: comment.errors.full_messages
+			}
+		end
   end
 
   def destroy
   end
+
+	private
+	def comment_params
+		params.require(:comment).permit(:board_id, :name, :comment)
+	end
+
 end
